039b8b1be47707c382c4cd4909b89b60b42d1779
Nathan Yang <nathan.yang16@gmail.com>
2021-10-09 15:11:45 -0400

    tested getting networks and getting containers by network from the frontend

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import './app.css';
 import IMAGE from './dog.png';
 
 export const App = () => {
-  const testExec = () => {
-    fetch('/api')
+  const testGetNetworks = () => {
+    fetch('/api/networks')
       .then((res) => res.json())
       .then((stuff) => console.log(stuff));
+  };
+
+  const testGetContainersByNetwork = () => {
+    fetch('api/containers/by-network/?networkName=bridge')
+      .then((res) => res.json())
+      .then((containers) => console.log(containers));
   };
 
   return (
     <>
       <h1 className="test">Fullstack React Typescript</h1>
       <img src={IMAGE} alt="dog" />
-      <button onClick={testExec}>Test Docker Terminal</button>
+      <button onClick={testGetNetworks}>Test Get Networks</button>
+      <button onClick={testGetContainersByNetwork}>Test Get Containers</button>
     </>
   );
 };
