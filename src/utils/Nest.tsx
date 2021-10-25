d5b0da4119d0649db615ce09a89705626921c2e7
Wyatt McMurry <wyattmcmurry1@gmail.com>
2021-10-25 11:07:53 -0400

    fixed spacing issue cuasing scroll bars in anmation

--- src/utils/Nest.tsx
+++ src/utils/Nest.tsx
@@ -31,6 +31,7 @@ export const Nest: React.FC<IProps> = ({
     backgroundColor: 'white',
     borderRadius: `${radius}%`,
     transition: 'border-radius 1s, width 1s, height 1s',
+    position: 'fixed',
     zIndex: 2,
   };
 
