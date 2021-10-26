41d3d97359bf21bc4c0142214b23afe2157c588b
Bernie Green <berniegreen@bernies-air.myfiosgateway.com>
2021-10-26 15:34:43 -0400

    update header logo to redirect to home page on click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import './header.scss';
-
+import { Link } from 'react-router-dom';
 interface IProps {
   toggleSideNav: () => void;
 }
@@ -14,14 +14,16 @@ export const Header: React.FC<IProps> = ({ toggleSideNav }) => {
         <div className="menuIcon" id="bar2"></div>
         <div className="menuIcon" id="bar3"></div>
       </button>
-      <div className="header-title">
-        <img
-          alt="anchor-logo"
-          className="header-logo"
-          src="http://localhost:3000/assets/ship-wheel.png"
-        ></img>
-        <div>DockerNet</div>
-      </div>
+      <Link className="homepage-link" to={`/`}>
+        <div className="header-title">
+          <img
+            alt="anchor-logo"
+            className="header-logo"
+            src="http://localhost:3000/assets/ship-wheel.png"
+          ></img>
+          <div>DockerNet</div>
+        </div>
+      </Link>
     </div>
   );
 };
